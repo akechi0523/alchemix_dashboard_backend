78fdc02a08058272147073ee5f4690fe7f907443
akechi0523 <ericlee1994523@gmail.com>
2024-05-22 01:33:38 +1000

    modifying index file

--- api/index.py
+++ api/index.py
@@ -5,17 +5,12 @@ from flask_cors import CORS
 from flask_cors import cross_origin
 
 app = Flask(__name__)
-CORS(app, resources={r"/*": {"origins": "http://localhost:3000"}})
-
+CORS(app, resources={r"/*": {"origins": "*"}})
 data = fetch_data()
 
 @app.route('/data')
 @cross_origin()
 def get_json_data():
-    # data = fetch_data()
-    #Read the JSON file and return its contents
-    # with open('data1.json') as f:
-    #     data = json.load(f)
     data1 = json.dumps(data, sort_keys=False)
     return data1
 
