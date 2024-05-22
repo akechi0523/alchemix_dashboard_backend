db6916aa17ec346f212481f6c21b8b777bbaa309
akechi0523 <ericlee1994523@gmail.com>
2024-05-22 13:00:29 +1000

    updating

--- api/index.py
+++ api/index.py
@@ -1,16 +1,18 @@
 import json
 from flask import Flask
-from .template.main_script import fetch_data
+# from .template.main_script import fetch_data
 from flask_cors import CORS
 from flask_cors import cross_origin
 
 app = Flask(__name__)
 CORS(app, resources={r"/*": {"origins": "*"}})
-data = fetch_data()
+# data = fetch_data()
 
 @app.route('/api/data')
 @cross_origin()
 def get_json_data():
+    with open("data1.json", 'r') as f:
+        data = json.load(f)
     data1 = json.dumps(data, sort_keys=False)
     return data1
 
