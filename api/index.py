db7e620e32468e94889dc9a1f33a34c7300efb83
akechi0523 <ericlee1994523@gmail.com>
2024-05-22 16:09:47 +1000

    updating

--- api/index.py
+++ api/index.py
@@ -1,18 +1,18 @@
 import json
 import time
 from flask import Flask
-# from .template.main_script import fetch_data
+from .template.main_script import fetch_data
 from flask_cors import CORS
 from flask_cors import cross_origin
 
 app = Flask(__name__)
 CORS(app, resources={r"/*": {"origins": "https://alchemix-dashboard-frontend.vercel.app/"}})
-# fetch_data()
+fetch_data()
 # time.sleep(60)
 @app.route('/api/data')
 @cross_origin()
 def get_json_data():
-    with open("data1.json", 'r') as f:
+    with open("data.json", 'r') as f:
         data = json.load(f)
     data1 = json.dumps(data, sort_keys=False)
     return data1
